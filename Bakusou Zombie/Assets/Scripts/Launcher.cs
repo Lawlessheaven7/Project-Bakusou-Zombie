ee0c0a3597d96406538bbc157a7cf88ecdac4716
Lawlessheaven7 <marcosxy70@gmail.com>
2022-09-26 00:26:03 -0400

    Improvement of Main Menu
    Almost done with main menu

--- Bakusou Zombie/Assets/Scripts/Launcher.cs
+++ Bakusou Zombie/Assets/Scripts/Launcher.cs
@@ -21,11 +21,18 @@ public class Launcher : MonoBehaviourPunCallbacks
     public GameObject createRoomScreen;
     public GameObject roomScreen;
     public GameObject errorScreen;
+    public GameObject roomBrowserScreen;
 
     public TMP_Text loadingText;
-    public TMP_InputField roomNameInput;
-    public TMP_Text roomText;
+    public TMP_Text roomText, playerNameLabel;
     public TMP_Text errorText;
+
+    public TMP_InputField roomNameInput;
+
+    public RoomButton theRoomButton;
+
+    private List<RoomButton> allRoomButtons = new List<RoomButton>();
+    private List<TMP_Text> allPlayerNames = new List<TMP_Text>();
 
     // Start is called before the first frame update
     void Start()
@@ -47,6 +54,7 @@ public class Launcher : MonoBehaviourPunCallbacks
         createRoomScreen.SetActive(false);
         roomScreen.SetActive(false);
         errorScreen.SetActive(false);
+        roomBrowserScreen.SetActive(false);
     }
 
     //Action that happens after connecting to the master server
@@ -63,6 +71,9 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         CloseMenus();
         menuButtons.SetActive(true);
+
+        //Temporally player name, will change later
+        PhotonNetwork.NickName = Random.Range(0, 1000f).ToString();
     }
 
     public void OpenRoomCreate()
@@ -96,6 +107,45 @@ public class Launcher : MonoBehaviourPunCallbacks
         roomScreen.SetActive(true);
 
         roomText.text = PhotonNetwork.CurrentRoom.Name;
+
+        ListAllPlayers();
+    }
+
+    //List all the players, get the information from the network and store it for display.
+    private void ListAllPlayers()
+    {
+        foreach(TMP_Text player in allPlayerNames)
+        {
+            Destroy(player.gameObject);
+        }
+        allPlayerNames.Clear();
+
+        Player[] players = PhotonNetwork.PlayerList;
+        for(int i = 0; i<players.Length; i++)
+        {
+            TMP_Text newPlayerLabel = Instantiate(playerNameLabel, playerNameLabel.transform.parent);
+            newPlayerLabel.text = players[i].NickName;
+            newPlayerLabel.gameObject.SetActive(true);
+
+            allPlayerNames.Clear();
+        }
+
+    }
+
+    //When a player joins the room display their name
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        TMP_Text newPlayerLabel = Instantiate(playerNameLabel, playerNameLabel.transform.parent);
+        newPlayerLabel.text = newPlayer.NickName;
+        newPlayerLabel.gameObject.SetActive(true);
+
+        allPlayerNames.Clear();
+    }
+
+    //Refresh and list all current players
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        ListAllPlayers();
     }
 
     // WHen creating the room failed
@@ -126,5 +176,60 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         CloseMenus();
         menuButtons.SetActive(true);
+    }
+
+    //Open the room browser
+    public void OpenRoomBrowser()
+    {
+        CloseMenus();
+        roomBrowserScreen.SetActive(true);
+    }
+
+    //Close the room browser
+    public void CloseRoomBrowser()
+    {
+        CloseMenus();
+        menuButtons.SetActive(true);
+    }
+
+    //Called at anytime when there is a change of the room while in the lobby.
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach(RoomButton rb in allRoomButtons)
+        {
+            Destroy(rb.gameObject);
+        }
+        allRoomButtons.Clear();
+
+        theRoomButton.gameObject.SetActive(false);
+
+        //Loop through all the roomlist information, then regulate the rooms.
+        for(int i = 0; i < roomList.Count; i++)
+        {
+            //As long as the players aren't full and it hasn't been removed from the list, then display the room.
+            if(roomList[i].PlayerCount != roomList[i].MaxPlayers && !roomList[i].RemovedFromList)
+            {
+                RoomButton newButton = Instantiate(theRoomButton, theRoomButton.transform.parent);
+                newButton.SetButtonDetails(roomList[i]);
+                newButton.gameObject.SetActive(true);
+
+                allRoomButtons.Add(newButton);
+            }
+        }
+    }
+    // Tell Photon Network to let player join the room after clicking the room name
+    public void JoinRoom(RoomInfo inputInfo)
+    {
+        PhotonNetwork.JoinRoom(inputInfo.Name);
+
+        CloseMenus();
+        loadingText.text = "Joining Room ";
+        loadingScreen.SetActive(true);
+    }
+
+    //Close the build Version of the game, does nothing to the editor version
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
