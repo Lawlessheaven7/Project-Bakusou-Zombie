842ffa2a773bd7148e9a4280c42ab478e64a2402
Lawlessheaven7 <marcosxy70@gmail.com>
2022-09-29 11:08:20 -0400

    Starter Asset Troubleshoot
    Eliminated the error causing starter asset to go wild in the console

---
+++ Bakusou Zombie/Assets/Scripts/PlayerInputErrorWorkaround.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputErrorWorkaround : MonoBehaviour
+{
+    private PlayerInput Input;
+
+    private void Start()
+    {
+        Input = GetComponent<PlayerInput>();
+    }
+
+    private void OnDisable()
+    {
+        Input.actions = null;
+    }
+}
+
